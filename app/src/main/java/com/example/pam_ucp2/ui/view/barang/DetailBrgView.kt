ff88320f29f2d992ca328024591edea60faec6c9
ulfhiraa <studyiraa@gmail.com>
2024-12-18 20:49:51 +0700

    ADD fungsi ItemDetailBrg untuk menampilkan informasi detail barang

--- app/src/main/java/com/example/pam_ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/barang/DetailBrgView.kt
@@ -1,8 +1,13 @@
 package com.example.pam_ucp2.ui.view.barang
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,10 +15,46 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pam_ucp2.data.entity.Barang
 
 @Composable
-fun ItemD
+fun ItemDetailBrg( // menampilkan informasi detail barang pada card
+    modifier: Modifier = Modifier,
+    barang: Barang
+){
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailBrg(judul = "Id", isinya = barang.id)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Nama", isinya = barang.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Deskripsi", isinya = barang.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Harga", isinya = barang.harga)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Stok", isinya = barang.stok)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Nama Supplier", isinya = barang.namaSupplier)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+
+    }
+}
 
 @Composable
 fun ComponentDetailBrg( // menampilkan detail informasi brgk
