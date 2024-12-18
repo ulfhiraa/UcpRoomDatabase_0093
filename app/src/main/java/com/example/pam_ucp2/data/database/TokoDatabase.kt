31e4795f984365cb98b43730aaf6e976cb83dde4
ulfhiraa <studyiraa@gmail.com>
2024-12-18 14:48:28 +0700

    ADD Abstract fun supplierDao untuk mengakses data Supplier

--- app/src/main/java/com/example/pam_ucp2/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/pam_ucp2/data/database/TokoDatabase.kt
@@ -3,6 +3,7 @@ package com.example.pam_ucp2.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.pam_ucp2.data.dao.BarangDao
+import com.example.pam_ucp2.data.dao.SupplierDao
 import com.example.pam_ucp2.data.entity.Barang
 import com.example.pam_ucp2.data.entity.Supplier
 
@@ -12,5 +13,8 @@ abstract class TokoDatabase : RoomDatabase(){
 
     // Mendefinisikan fungsi untuk mengakses data Barang
     abstract fun barangDao(): BarangDao
+
+    // Mendefinisikan fungsi untuk mengakses data Supplier
+    abstract fun supplierDao(): SupplierDao
 
 }
