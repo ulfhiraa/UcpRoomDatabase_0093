3d01d269f61f50df06bad86a1916f9a547ee44ab
ulfhiraa <studyiraa@gmail.com>
2024-12-18 22:25:16 +0700

    ADD Class BarangViewModel untuk mengelola input, validasi, penyimpanan data, dan pengaturan snackbar.

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,7 +1,79 @@
 package com.example.pam_ucp2.ui.viewmodel.barang
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_ucp2.data.entity.Barang
+import com.example.pam_ucp2.data.repository.RepositoryBrg
+import kotlinx.coroutines.launch
+
+//  untuk mengelola input data barang, validasi form, penyimpanan data ke database, dan pengaturan pesan snackbar.
+class BarangViewModel(private val repositoryBrg: RepositoryBrg) : ViewModel(){
+
+    var uiState by mutableStateOf(BrgUIState()) // event dapat berubah
+
+    // memperbarui state berdasarkan input pengguna
+    // untuk mengubah tampilan di textfield
+    fun updateState(barangEvent: BarangEvent) {
+        uiState = uiState.copy(
+            barangEvent = barangEvent,
+        )
+    }
+
+    // validasi data input pengguna
+    private fun validateFields(): Boolean{
+        val event = uiState.barangEvent
+        val errorState = FormErrorState(
+            id = if (event.id.isNotEmpty()) null else "ID tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi tidak boleh kosong",
+            harga = if (event.harga.isNotEmpty()) null else "Harga tidak boleh kosong",
+            stok = if (event.stok.isNotEmpty()) null else "Stok tidak boleh kosong",
+            namaSupplier = if (event.namaSupplier.isNotEmpty()) null else "Nama Supplier tidak boleh kosong",
+        )
+
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    // menyimpan data ke repository
+    // menyimpan data ke database. dao untuk menghashnya melalui sebuah repository
+    fun saveData() {
+        val currentEvent = uiState.barangEvent
+
+        if (validateFields()) { // kalau datanya benar, tidak kosong
+            viewModelScope.launch { // memunculkan view model scope
+                try { // menggunakan try agar kita tahu errornya apa
+                    repositoryBrg.insertBrg(currentEvent.toBarangEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        barangEvent = BarangEvent(), // Reset input form
+                        isEntryValid = FormErrorState() // Reset error state
+                    )
+                } catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+
+    // Reset pesan Snackbar setelah ditampilkan
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
 // event adalah aksi yang merubah kondisi
 // state adalah keadaan yang terjadi setelah ada trigger dari event
+// Menambahkan data class BrgUIState untuk merepresentasikan state UI, termasuk event, validasi input, dan pesan snackbar.
 data class BrgUIState(
     val barangEvent: BarangEvent = BarangEvent(),
     val isEntryValid: FormErrorState = FormErrorState(),
