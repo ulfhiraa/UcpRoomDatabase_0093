81c35c56d5d499993059beb6a8ab3494e0cc3e83
ulfhiraa <studyiraa@gmail.com>
2024-12-18 14:01:37 +0700

    ADD Entitas Supplier

---
+++ app/src/main/java/com/example/pam_ucp2/data/entity/Supplier.kt
@@ -0,0 +1,13 @@
+package com.example.pam_ucp2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "supplier")
+data class Supplier(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val kontak: String,
+    val alamat: String
+)
