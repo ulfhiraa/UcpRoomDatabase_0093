bb984dd4289e8e1a8342110f7d515dfff04593d3
ulfhiraa <studyiraa@gmail.com>
2024-12-18 22:58:23 +0700

    ADD Class HomeBrgViewModel untuk mengelola status UI pada tampilan utama mahasiswa, termasuk menangani status loading, error, dan data mahasiswa.

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/HomeBrgViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/HomeBrgViewModel.kt
@@ -1,6 +1,55 @@
 package com.example.pam_ucp2.ui.viewmodel.barang
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pam_ucp2.data.entity.Barang
+import com.example.pam_ucp2.data.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+/*
+untuk mengelola status UI pada tampilan utama mahasiswa,
+termasuk menangani status loading, error, dan data mahasiswa.
+*/
+class HomeBrgViewModel (
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel(){
+    val homeUiState: StateFlow<HomeUiState> = repositoryBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+
+}
 
 // state; mengubah tampilan
 data class HomeUiState(
