e0c2369b6ab03229f7ecc2df7e2989c7713b1d19
ulfhiraa <studyiraa@gmail.com>
2024-12-18 14:31:53 +0700

    ADD Barang DAO

---
+++ app/src/main/java/com/example/pam_ucp2/data/dao/BarangDao.kt
@@ -0,0 +1,34 @@
+package com.example.pam_ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pam_ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BarangDao {
+    @Insert
+    suspend fun insertBarang(
+        barang: Barang
+    )
+
+    @Query("SELECT * FROM barang ORDER BY nama ASC")
+    fun getAllBarang(): Flow<List<Barang>>
+
+    @Query("SELECT * FROM barang WHERE id = :id")
+    fun getBarang (id: String) : Flow<Barang>
+
+    @Delete
+    suspend fun deleteBarang(
+        barang: Barang
+    )
+
+    @Update
+    suspend fun updateBarang(
+        barang: Barang
+    )
+}
+
