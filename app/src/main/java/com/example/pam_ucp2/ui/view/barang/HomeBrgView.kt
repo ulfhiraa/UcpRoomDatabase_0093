ef3b1d9f9450ac4ecc110402768460aa6082783a
ulfhiraa <studyiraa@gmail.com>
2024-12-18 22:49:44 +0700

    ADD Fungsi BodyHomeBrgView  untuk menampilkan status UI (loading, error, data kosong, atau daftar barang) dan Snackbar.

--- app/src/main/java/com/example/pam_ucp2/ui/view/barang/HomeBrgView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/barang/HomeBrgView.kt
@@ -1,8 +1,10 @@
 package com.example.pam_ucp2.ui.view.barang
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,16 +14,83 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pam_ucp2.data.entity.Barang
+import kotlinx.coroutines.launch
+
+@Composable //  untuk menampilkan status UI (loading, error, data kosong, atau daftar barang dengan dukungan tampilan dinamis dan Snackbar.
+fun BodyHomeBrgView( // untuk menampilkan data
+    homeUiState: HomeUiState, // berisi status UI. apakah data sedang loading, ada error atau sudah ada data
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier // mengatur properti layout
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar state
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            // menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data Barang.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar barang
+            ListBarang(
+                ListBrg = homeUiState.listbrg,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListBarang( // menampilkan daftar barang
