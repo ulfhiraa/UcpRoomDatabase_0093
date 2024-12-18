0fd577121d2eaed0e3c3f38e85beb32ce03bc28e
ulfhiraa <studyiraa@gmail.com>
2024-12-18 22:17:09 +0700

    ADD Data Class FormErrorState untuk memberikan validasi data benar atau salah

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,5 +1,19 @@
 package com.example.pam_ucp2.ui.viewmodel.barang
 
+// untuk menghandle atau memberikan nilai validasi apakah data benar atau tidak
+data class FormErrorState(
+    val id: String? = null,
+    val nama: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val namaSupplier: String? = null
+) {
+    fun isValid(): Boolean {
+        return id == null && nama == null && deskripsi == null &&
+                harga == null && stok == null && namaSupplier == null
+    }
+}
 
 // data class variabel yang menyimpan data input form
 data class BarangEvent(
