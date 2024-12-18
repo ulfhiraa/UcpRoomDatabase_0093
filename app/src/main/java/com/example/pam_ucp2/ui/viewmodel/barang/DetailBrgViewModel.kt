ac76510ae8f37f52996458b90042cd28bc22c93f
ulfhiraa <studyiraa@gmail.com>
2024-12-18 21:50:43 +0700

    ADD data class DetailUiState untuk menampung data yang akan ditampilkan di UI

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -3,6 +3,20 @@
 Data Class untuk menampung data yang akan ditampilkan di UI
 * */
 
+data class DetailUiState(
+    val detailUiEvent: BarangEvent = BarangEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == BarangEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != BarangEvent()
+}
+
+
 // memindahkan data dari entity ke ui
 fun Barang.toDetailUiEvent(): BarangEvent {
     return BarangEvent(
