5f832fefad2f63e5eb915aca8ff8262da43f46b5
ulfhiraa <studyiraa@gmail.com>
2024-12-18 23:58:16 +0700

    ADD fun UpdateBrgView untuk memperbarui informasi barang

--- app/src/main/java/com/example/pam_ucp2/ui/view/barang/UpdateBrgView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/barang/UpdateBrgView.kt
@@ -1,10 +1,90 @@
 package com.example.pam_ucp2.ui.view.barang
 
-import com.example.pam_ucp2.data.entity.Barang
-import com.example.pam_ucp2.ui.viewmodel.barang.BrgUIState
-import toDetailUiEvent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2.ui.customwidget.TopAppBar
+import com.example.pam_ucp2.ui.viewmodel.PenyediaViewModel
+import com.example.pam_ucp2.ui.viewmodel.barang.UpdateBrgViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// untuk konversi objek Barang ke UIState Brg.
-fun Barang.toUIStateBrg() : BrgUIState = BrgUIState(
-    barangEvent = this.toDetailUiEvent(),
-)
+@Composable
+fun UpdateBrgView( // untuk memperbarui informasi barang
+    onBack: () -> Unit,
+    onNavigasi: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateBrgViewModel = viewModel(factory = PenyediaViewModel.Factory) // inisialisasi ViewModel
+){
+    val uiState = viewModel.updateUIState // ambil UI state dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching Coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = Modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }, // Tempatkan snackbar di scaffold
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Edit Barang",
+                modifier = modifier,
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // isi body
+            InsertBodyBrg(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) // update state di ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main){
+                                onNavigasi() // navigasi di main thread
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
