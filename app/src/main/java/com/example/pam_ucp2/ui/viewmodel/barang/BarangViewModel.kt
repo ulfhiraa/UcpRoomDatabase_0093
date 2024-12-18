33c9dd1edf210b9546c3dfffce4283da3563192f
ulfhiraa <studyiraa@gmail.com>
2024-12-18 22:15:20 +0700

    ADD Data class BarangEvent untuk menyimpan data input form

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.pam_ucp2.ui.viewmodel.barang
 
 
+// data class variabel yang menyimpan data input form
+data class BarangEvent(
+    val id: String = "",
+    val nama: String = "",
+    val deskripsi: String = "",
+    val harga: String = "",
+    val stok: String = "",
+    val namaSupplier: String = ""
+)
+
 // Menyimpan input form ke dalam entity
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     id = id,
