920632992977af16ef6c59f535ba4f7d624aac71
ulfhiraa <studyiraa@gmail.com>
2024-12-18 14:32:16 +0700

    ADD Supplier DAO

---
+++ app/src/main/java/com/example/pam_ucp2/data/dao/SupplierDao.kt
@@ -0,0 +1,31 @@
+package com.example.pam_ucp2.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pam_ucp2.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+interface SupplierDao {
+    @Insert
+    suspend fun insertSupplier(
+        supplier: Supplier
+    )
+
+    @Query("SELECT * FROM supplier ORDER BY nama ASC")
+    fun getAllSupplier() : Flow<List<Supplier>>
+
+    @Query("SELECT * FROM supplier WHERE id = :id")
+    fun getSupplier (id: String) : Flow<Supplier>
+
+    @Delete
+    suspend fun deleteSupplier(
+        supplier: Supplier
+    )
+
+    @Update
+    suspend fun updateSupplier(
+        supplier: Supplier
+    )
+}
