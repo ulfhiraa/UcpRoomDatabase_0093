7dbd52006e388412ca2f6d9a924560918c0cba0f
ulfhiraa <studyiraa@gmail.com>
2024-12-18 14:47:39 +0700

    ADD Abstract fun barangDao untuk mengakses data Barang

--- app/src/main/java/com/example/pam_ucp2/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/pam_ucp2/data/database/TokoDatabase.kt
@@ -2,11 +2,15 @@ package com.example.pam_ucp2.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.pam_ucp2.data.dao.BarangDao
 import com.example.pam_ucp2.data.entity.Barang
 import com.example.pam_ucp2.data.entity.Supplier
 
 // mendefinisikan database dengan tabel Barang dan Supplier
 @Database(entities = [Barang::class, Supplier::class], version = 2, exportSchema = false)
 abstract class TokoDatabase : RoomDatabase(){
+
+    // Mendefinisikan fungsi untuk mengakses data Barang
+    abstract fun barangDao(): BarangDao
 
 }
