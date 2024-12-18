9b7524ab7903eaacd34463549a0b7f6f6ca2c7f9
ulfhiraa <studyiraa@gmail.com>
2024-12-18 20:34:01 +0700

    ADD fungsi DeleteConfirmationDialog untuk konfirmasi sebelum menghapus data

---
+++ app/src/main/java/com/example/pam_ucp2/ui/view/barang/DetailBrgView.kt
@@ -0,0 +1,30 @@
+package com.example.pam_ucp2.ui.view.barang
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog( // Konfirmasi penghapusan data
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda yakin ingin menghapus data? ") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
