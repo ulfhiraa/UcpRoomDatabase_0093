926539f7257773d142001c7d0872b2d8df241808
ulfhiraa <studyiraa@gmail.com>
2024-12-19 15:20:33 +0700

    ADD fungsi InsertBrgView untuk menampilkan form input barang dengan snackbar

--- app/src/main/java/com/example/pam_ucp2/ui/view/barang/InsertBrgView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/barang/InsertBrgView.kt
@@ -3,21 +3,85 @@ package com.example.pam_ucp2.ui.view.barang
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2.ui.customwidget.TopAppBar
+import com.example.pam_ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.pam_ucp2.ui.viewmodel.barang.BarangEvent
+import com.example.pam_ucp2.ui.viewmodel.barang.BarangViewModel
 import com.example.pam_ucp2.ui.viewmodel.barang.BrgUIState
 import com.example.pam_ucp2.ui.viewmodel.barang.FormErrorState
+import kotlinx.coroutines.launch
 
+@Composable
+fun InsertBrgView( // untuk menampilkan form input barang dengan snackbar
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: BarangViewModel = viewModel(factory = PenyediaViewModel.Factory) // Inisialisasi viewModel
+){
+    val uiState = viewModel.uiState // ambil UI state dari viewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                //.padding(padding)
+                .padding(16.dp)
+        ){
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Barang",
+                modifier = modifier
+            )
+            // Isi Body
+            InsertBodyBrg(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) // update state di ViewModel
+                },
+                onClick = {
+                    viewModel.saveData()
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodyBrg( // Menambahkan tampilan form untuk memasukkan data barang dan button simpan.
