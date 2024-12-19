775073b34bc3e00ea0a5909cedbba4f360b340af
ulfhiraa <studyiraa@gmail.com>
2024-12-19 15:36:03 +0700

    ADD fungsi SupplierEvent.toSupplierEntity() untuk menyimpan input form ke dalam entity

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.pam_ucp2.ui.viewmodel.supplier
 
+import com.example.pam_ucp2.data.entity.Supplier
+
+
+
+// Menyimpan input form ke dalam entity
+fun SupplierEvent.toSupplierEntity(): Supplier = Supplier(
+    id = id,
+    nama = nama,
+    kontak = kontak,
+    alamat = alamat
+)
