50be10c3a017abf7343098cba5a3d2dd2b58d431
ulfhiraa <studyiraa@gmail.com>
2024-12-19 15:24:56 +0700

    ADD import DestinasiInsertBrg

--- app/src/main/java/com/example/pam_ucp2/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/navigasi/PengelolaHalaman.kt
@@ -2,27 +2,28 @@ package com.example.pam_ucp2.ui.navigasi
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.pam_ucp2.ui.view.PilihanHomeView
+import com.example.pam_ucp2.ui.view.barang.DestinasiInsertBrg
 import com.example.pam_ucp2.ui.view.barang.DetailBrgView
 import com.example.pam_ucp2.ui.view.barang.HomeBrgView
+import com.example.pam_ucp2.ui.view.barang.InsertBrgView
 import com.example.pam_ucp2.ui.view.barang.UpdateBrgView
 
 @Composable
 fun PengelolaHalaman( // untuk mengelola navigasi antar halaman home, barang dan supplier
-    navController: NavController = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ){
-    NavHost(
+    NavHost (
         navController = navController,
-        startDestination = DestinasiMain.route // mulai dari halaman pilihan home
-    ) {
-        // Halaman Pilihan HOME
+        startDestination = DestinasiMain.route) {
+
         composable(
             route = DestinasiMain.route
         ){
