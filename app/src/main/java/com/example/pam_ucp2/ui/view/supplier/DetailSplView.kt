2f81332f088c586f597d929df72c910505d12690
ulfhiraa <studyiraa@gmail.com>
2024-12-20 12:23:16 +0700

    DELETE tombol edit dan interaksi lainnya, hanya menampilkan detail supplier

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/DetailSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/DetailSplView.kt
@@ -7,24 +7,60 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam_ucp2.data.entity.Supplier
+import com.example.pam_ucp2.ui.customwidget.TopAppBar
+import com.example.pam_ucp2.ui.view.barang.BodyDetailBrg
 import com.example.pam_ucp2.ui.view.barang.ComponentDetailBrg
+import com.example.pam_ucp2.ui.viewmodel.PenyediaViewModel
+import com.example.pam_ucp2.ui.viewmodel.supplier.DetailSplViewModel
 import com.example.pam_ucp2.ui.viewmodel.supplier.toSupplierEntity
 
+@Composable //untuk menampilkan detail supplier
+fun DetailSplView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailSplViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { }
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Detail Supplier",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
 
-@Composable // untuk tampilan untuk detail supplier dengan kondisi loading, data ditemukan, dan data kosong
+        BodyDetailSpl(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState
+        )
+    }
+}
+
+@Composable // Untuk tampilan detail supplier dengan kondisi loading, data ditemukan, dan data kosong
 fun BodyDetailSpl(
     modifier: Modifier = Modifier,
     detailUiState: DetailUiState = DetailUiState(),
@@ -83,13 +119,13 @@ fun ItemDetailSpl( // menampilkan informasi detail supplier pada card
         Column (
             modifier = Modifier.padding(16.dp)
         ) {
-            ComponentDetailBrg(judul = "Nama", isinya = supplier.nama)
+            ComponentDetailSpl(judul = "Nama", isinya = supplier.nama)
             Spacer(modifier = Modifier.padding(4.dp))
 
-            ComponentDetailBrg(judul = "Kontak", isinya = supplier.kontak)
+            ComponentDetailSpl(judul = "Kontak", isinya = supplier.kontak)
             Spacer(modifier = Modifier.padding(4.dp))
 
-            ComponentDetailBrg(judul = "Alamat", isinya = supplier.alamat)
+            ComponentDetailSpl(judul = "Alamat", isinya = supplier.alamat)
             Spacer(modifier = Modifier.padding(4.dp))
         }
     }
