8114b015e4368bf0c63e35810dabc77a7dec50cc
ulfhiraa <studyiraa@gmail.com>
2024-12-20 12:16:31 +0700

    ADD fungsi BodyDetailSpl untuk tampilan untuk detail supplier dengan kondisi loading, data ditemukan, dan data kosong

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/DetailSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/DetailSplView.kt
@@ -1,11 +1,15 @@
 package com.example.pam_ucp2.ui.view.supplier
 
+import DetailUiState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +21,52 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pam_ucp2.data.entity.Supplier
 import com.example.pam_ucp2.ui.view.barang.ComponentDetailBrg
+import com.example.pam_ucp2.ui.viewmodel.supplier.toSupplierEntity
+
+
+@Composable // untuk tampilan untuk detail supplier dengan kondisi loading, data ditemukan, dan data kosong
+fun BodyDetailSpl(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+) {
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // tampilkan loading
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            val supplier = detailUiState.detailUiEvent.toSupplierEntity()
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailSpl(
+                    supplier = supplier,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailSpl( // menampilkan informasi detail supplier pada card
