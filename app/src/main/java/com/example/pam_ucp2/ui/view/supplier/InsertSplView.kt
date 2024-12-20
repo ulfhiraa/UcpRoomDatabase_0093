d2fe6c47a65ea562c21c074e03d3f60e6a8d2fa1
ulfhiraa <studyiraa@gmail.com>
2024-12-20 14:31:06 +0700

    ADD Fungsi InsertBodySpl untuk memasukkan data supplier dan button simpan.

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/InsertSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/InsertSplView.kt
@@ -1,20 +1,51 @@
 package com.example.pam_ucp2.ui.view.supplier
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pam_ucp2.ui.viewmodel.supplier.FormErrorState
+import com.example.pam_ucp2.ui.viewmodel.supplier.SplUIState
 import com.example.pam_ucp2.ui.viewmodel.supplier.SupplierEvent
+
+@Composable
+fun InsertBodySpl( // Menambahkan tampilan form untuk memasukkan data supplier dan button simpan.
+    modifier: Modifier = Modifier,
+    onValueChange: (SupplierEvent) -> Unit,
+    uiState: SplUIState,
+    onClick: () -> Unit
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormSupplier(
+            supplierEvent = uiState.supplierEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable // untuk menampilkan input form textfield data Supplier
