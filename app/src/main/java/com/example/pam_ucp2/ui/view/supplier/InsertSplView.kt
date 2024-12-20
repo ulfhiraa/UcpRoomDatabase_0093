d922489e2af782af96348acbafe28b4da3eadec1
ulfhiraa <studyiraa@gmail.com>
2024-12-20 14:29:20 +0700

    ADD FormSupplier untuk menampilkan input form textfield data Supplier

---
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/InsertSplView.kt
@@ -0,0 +1,82 @@
+package com.example.pam_ucp2.ui.view.supplier
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pam_ucp2.ui.viewmodel.supplier.FormErrorState
+import com.example.pam_ucp2.ui.viewmodel.supplier.SupplierEvent
+
+@Preview(showBackground = true)
+@Composable // untuk menampilkan input form textfield data Supplier
+fun FormSupplier(
+    supplierEvent: SupplierEvent = SupplierEvent(),
+    onValueChange: (SupplierEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    Column() {
+        // TEXTFIELD NAMA
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.nama,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan nama") },
+        )
+        Text( // validasi error pada text field
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        // TEXTFIELD KONTAK
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.kontak,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(kontak = it))
+            },
+            label = { Text("Kontak") },
+            isError = errorState.kontak != null,
+            placeholder = { Text("Masukkan kontak") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+            )
+        )
+        Text( // validasi error pada text field
+            text = errorState.kontak ?: "",
+            color = Color.Red
+        )
+
+        // TEXTFIELD ALAMAT
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = supplierEvent.alamat,
+            onValueChange = {
+                onValueChange(supplierEvent.copy(alamat = it))
+            },
+            label = { Text("Alamat") },
+            isError = errorState.alamat != null,
+            placeholder = { Text("Masukkan alamat") },
+        )
+        Text( // validasi error pada text field
+            text = errorState.alamat ?: "",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Alamat")
+    }
+}
