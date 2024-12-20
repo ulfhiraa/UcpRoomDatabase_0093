a72d763779439e2655a0b00f2db94d504847dbe1
ulfhiraa <studyiraa@gmail.com>
2024-12-20 14:21:30 +0700

    DELETE import yang tidak digunakan pada HomeSplView

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/HomeSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/HomeSplView.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
