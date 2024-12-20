df83b518528477a85a20221aa7c1e8dee4cb0f24
ulfhiraa <studyiraa@gmail.com>
2024-12-20 14:32:52 +0700

    ADD fun InsertSplView  untuk menampilkan form input supplier dengan snackbar

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/InsertSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/InsertSplView.kt
@@ -3,22 +3,87 @@ package com.example.pam_ucp2.ui.view.supplier
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2.ui.customwidget.TopAppBar
+import com.example.pam_ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.pam_ucp2.ui.viewmodel.supplier.FormErrorState
 import com.example.pam_ucp2.ui.viewmodel.supplier.SplUIState
 import com.example.pam_ucp2.ui.viewmodel.supplier.SupplierEvent
+import com.example.pam_ucp2.ui.viewmodel.supplier.SupplierViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertSplView( // untuk menampilkan form input supplier dengan snackbar
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: SupplierViewModel = viewModel(factory = PenyediaViewModel.Factory) // Inisialisasi viewModel
+){
+    val uiState = viewModel.uiState // ambil UI state dari viewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                //.padding(padding)
+                .padding(16.dp)
+        ){
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Supplier",
+                modifier = modifier
+            )
+            // Isi Body
+            InsertBodySpl(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent) // update state di ViewModel
+                },
+                onClick = {
+                    viewModel.saveData()
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodySpl( // Menambahkan tampilan form untuk memasukkan data supplier dan button simpan.
