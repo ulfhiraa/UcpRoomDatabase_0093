6738276a09fc83102b01d692ad73b93b1174ff66
ulfhiraa <studyiraa@gmail.com>
2024-12-20 11:51:06 +0700

    ADD data class DetailUiState untuk menampung data yang akan ditampilkan di UI Supplier

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/DetailSplViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/DetailSplViewModel.kt
@@ -1,5 +1,21 @@
 package com.example.pam_ucp2.ui.viewmodel.supplier
 
+import com.example.pam_ucp2.data.entity.Supplier
+
+// Data Class untuk menampung data yang akan ditampilkan di UI
+data class DetailUiState(
+    val detailUiEvent: SupplierEvent = SupplierEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == SupplierEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != SupplierEvent()
+}
+
 // memindahkan data dari entity ke ui
 fun Supplier.toDetailUiEvent(): SupplierEvent {
     return SupplierEvent(
