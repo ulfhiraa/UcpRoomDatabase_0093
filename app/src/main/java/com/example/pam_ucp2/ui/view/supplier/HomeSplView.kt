c6d39b5389538179a4194a82f1dd91adf9adaaad
ulfhiraa <studyiraa@gmail.com>
2024-12-20 14:06:07 +0700

    ADD fungsi CardSpl  untuk menampilkan informasi supplier(nama, kontak, alamat) dengan mengklik card

---
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/HomeSplView.kt
@@ -0,0 +1,95 @@
+package com.example.pam_ucp2.ui.view.supplier
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pam_ucp2.data.entity.Supplier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardSpl( // untuk menampilkan informasi supplier(nama, kontak, alamat) dengan mengklik card
+    spl: Supplier,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            // nama supplier
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+
+            // id supplier
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.id.toString(), // agar tipe data numerik dapat dikonversi menjadi String
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+
+            // kontak supplier
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.kontak,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+
+            // Alamat supplier
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = spl.alamat.toString(), // agar tipe data numerik dapat dikonversi menjadi String
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
