47af14abcdab7db984f3f5014788b621ebdea1ae
ulfhiraa <studyiraa@gmail.com>
2024-12-20 11:31:00 +0700

    ADD data class FormErrorState untuk menghandle validasi nilai data

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/supplier/SupplierViewModel.kt
@@ -2,7 +2,24 @@ package com.example.pam_ucp2.ui.viewmodel.supplier
 
 import com.example.pam_ucp2.data.entity.Supplier
 
+// untuk menghandle atau memberikan nilai validasi apakah data benar atau tidak
+data class FormErrorState(
+    val nama: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null,
+) {
+    fun isValid(): Boolean {
+        return nama == null && kontak == null &&  alamat == null
+    }
+}
 
+// data class variabel yang menyimpan data input form
+data class SupplierEvent(
+    val id: Int,
+    val nama: String = "",
+    val kontak: String = "",
+    val alamat: String = ""
+)
 
 // Menyimpan input form ke dalam entity
 fun SupplierEvent.toSupplierEntity(): Supplier = Supplier(
