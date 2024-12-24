4dcf79770d9fbaddd29d90e85e8df0785430b56a
ulfhiraa <studyiraa@gmail.com>
2024-12-24 08:17:27 +0700

    DELETE import yang tidak diperlukan pada HomeSplView.kt

--- app/src/main/java/com/example/pam_ucp2/ui/view/supplier/HomeSplView.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/supplier/HomeSplView.kt
@@ -18,8 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
